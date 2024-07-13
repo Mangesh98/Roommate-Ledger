7c2d30c4f5e6673e569ec26c7dc5e109546158ba
Mangesh98 <mangeshraje55555@gmail.com>
2024-07-13 13:24:13 +0530

    Production issue fix

--- server/app.js
+++ server/app.js
@@ -8,8 +8,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-	// origin: [process.env.CLIENT_URL], // Next.js app's URL
-	origin: [process.env.CLIENT_URL_REACT], // React app's URL
+	origin: [process.env.CLIENT_URL], // Next.js app's URL
+	// origin: [process.env.CLIENT_URL_REACT], // React app's URL
 	credentials: true, // Allow cookies for authentication
 	allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
 	methods: "GET, POST", // Allowed HTTP methods
